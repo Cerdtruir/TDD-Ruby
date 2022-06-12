db1c7e431cd0d8bada3f18d5661fce4adf915259
Cerdtruir <ghoornabeel@gmail.com>
2022-06-12 20:40:05 +0200

    Add factorial test

---
+++ spec/solver_spec.rb
@@ -0,0 +1,15 @@
+require('../solver')
+
+describe Solver do
+  context 'factorial method works correctly' do
+    it 'returns the correct value if integer is positive' do
+      expect(Solver.new.factorial(4)).to eq 24
+    end
+    it 'returns 1 if integer is zero' do
+      expect(Solver.new.factorial(0)).to eq 1
+    end
+    it 'returns error if integer is negative' do
+      expect(Solver.new.factorial(-20)).to be 'Error: Negative input number'
+    end
+  end
+end
